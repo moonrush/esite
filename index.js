e5519e267adead7f74bc0a75d9a9ca87843762d5
Daniel <lizhe.0625@gmail.com>
2015-06-04 15:53:00 +0800

    Initial commit again

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 var express = require('express');
 var app = express();
+
+//设置handlebars视图引擎
+var handlebars = require('express3-handlebars')
+	.create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine','handlebars');
+
 app.set('port',process.env.PORT || 3000);
 
-//定制404页面
-app.use(function(req.res){
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not Found');
-});
+/**--------------------------------*/
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
 
+//将public中的所有内容加载为static中间件
+app.use(express.static(__dirname + '/public'));
+
+//定义路由
+app.get('/', function(req,res){
+	res.render('home');
+});
+app.get('/about', function(req,res){
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about',{fortune: randomFortune});
+})
+
+//定制404页面
+app.use(function(req,res){
+	res.status(404);
+	res.render('404');
+});
 //定制500页面
 app.use(function(err,req,res,next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
-)};
+	res.render('500');
+});
 
 app.listen(app.get('port'),function(){
 	console.log('Express started on http://localhost:' + app.get('port') +
